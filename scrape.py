d43104c2bc96fb4fe97bc2f719528abd2b5864a5
Abhiram Sunkara <111587552+aksunk01@users.noreply.github.com>
2024-06-28 13:57:34 -0400

    Add files via upload
    this has more functions and the code works to where you could get the full article when you have multiple links

--- scrape.py
+++ scrape.py
@@ -1,29 +1,52 @@
 from bs4 import BeautifulSoup
 import requests
 import re
-'''url = "https://www.cnn.com/politics"'''
-url = "https://www.cnn.com/2024/06/26/politics/supreme-court-abortion-idaho-bloomberg/index.html"
-results = requests.get(url)
-doc = BeautifulSoup(results.text, "html.parser")
+
+def getArticleLinks():
+    url = "https://www.cnn.com/politics"
+    results = requests.get(url)
+    doc = BeautifulSoup(results.text, "html.parser")
+
+    '''This gets the headlines for the politics page of CNN'''
+    href = doc.find_all("div", class_="container__field-links container_lead-plus-headlines__field-links")
+    links = []
+    beginning  = "https://www.cnn.com"
+
+    '''This gets and formats the links from the provided headlines'''
+    for i in href:
+        end = str(i.find("a").get("href"))
+        
+        link = beginning + end
+        links.append(link)
+    return(links)
+
+def newsArticle(link):
+    url = link
+    results = requests.get(url)
+    doc = BeautifulSoup(results.text, "html.parser")
+    page = doc.find_all("p")
+
+    article_body = ""
+    
+    for i in page:
+        article_body+=i.text
+
+    return (article_body)
+
+
+
+   
+for i in getArticleLinks():
+    print(newsArticle(i))
+
+
 
 '''
-
-href = doc.find_all("div", class_="container__field-links container_lead-plus-headlines__field-links")
-links = []
-beginning = "https://www.cnn.com"
-
-for i in href:
-    end = str(i.find("a").get("href"))
-    link = beginning + end
-    links.append(link)
-
-
-
 def newsText(link):
     url = link
     print(url)
 
-'''
+
 page = doc.find_all("p")
 
 article = ""
@@ -31,8 +54,5 @@ article = ""
 for i in page:
     article+=i.text
 
-
-
-
-
-
+print(article)
+'''
